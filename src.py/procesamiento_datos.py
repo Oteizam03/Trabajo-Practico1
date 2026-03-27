bf9c42560fd205952e7b58ce6917a88959083123
delfina-ferrero <dferrero@udesa.edu.ar>
2026-03-27 12:33:02 -0300

    procesamiento update

--- src.py/procesamiento_datos.py
+++ src.py/procesamiento_datos.py
@@ -6,4 +6,13 @@ Created on Thu Mar 26 15:23:43 2026
 @author: milagrosoteiza
 """
 
-procesamineto_datos
+
+from cargar_datos_1 import parsear_linea, cargar_datos
+
+def filtrar_por_participante(datos: list, id_participante: int):
+    linea = parsear_linea()
+    datos = cargar_datos(linea)
+    for dic in datos:
+        if id_participante == dic["id_participante"]:
+            return dic
+        
