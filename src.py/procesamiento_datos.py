7bf70b29ee729bff8340fa9a9f07396d34950f2a
delfina-ferrero <dferrero@udesa.edu.ar>
2026-04-13 13:12:53 -0300

    Update procesamiento_datos.py

--- src.py/procesamiento_datos.py
+++ src.py/procesamiento_datos.py
@@ -10,6 +10,22 @@ Created on Thu Mar 26 15:23:43 2026
 from cargar_datos_1 import parsear_linea, cargar_datos
 
 def filtrar_por_participante(datos: list, id_participante: int):
+    '''
+    se encarga de seleccionar los datos correspondientes de un alumno y devolverlos en un diccionario
+
+    Parameters
+    ----------
+    datos : list
+        es una lista que contiene cada registro de la funcion a la que llama. quedaria cada registro de cada individuo..
+    id_participante : int
+        identiicador del participante.
+
+    Returns
+    -------
+    dic : TYPE
+        diccionario que contiene los datos correspondientes a un alumno.
+
+    '''
     linea = parsear_linea()
     datos = cargar_datos(linea)
     for dic in datos:
